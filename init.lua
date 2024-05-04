e563d45681f0fb3db0ada18c5b52c3724143a58b
Muhammad Ashir <mashir670@gmail.com>
2024-05-04 19:30:12 +0500

    ashir's fuckery and catppuccin

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
---[[
+--[[init
 
 ===================================================================== ==================== READ THIS BEFORE CONTINUING ====================
 =====================================================================
@@ -231,12 +231,24 @@ require('lazy').setup({
 
 
   {
-    "folke/tokyonight.nvim",
-    lazy = false,
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
-    opts = {},
+    opts = {
+      transparent_background = true,
+      integrations = {
+        telescope = true,
+        harpoon = true,
+        mason = true,
+        neotest = true,
+      }
+    },
+    config = function(_, opts)
+      require('catppuccin').setup(opts)
+      vim.cmd.colorscheme 'catppuccin-frappe'
+    end
   },
-  
+
   {
     "folke/zen-mode.nvim",
   },
@@ -260,7 +272,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'tokyonight',
+        theme = 'catppuccin',
         component_separators = '|',
         section_separators = '',
       },
@@ -553,34 +565,7 @@ vim.defer_fn(function()
 end, 0)
 
 
-  -- Configure tokyonight
-  require("tokyonight").setup({
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-    light_style = "day", -- The theme is used when the background is set to light
-    transparent = true, -- Enable this to disable setting the background color
-    terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
-    styles = {
-      -- Style to be applied to different syntax groups
-      -- Value is any valid attr-list value for `:help nvim_set_hl`
-      comments = { italic = true },
-      keywords = { italic = true },
-      functions = {},
-      variables = {},
-      -- Background styles. Can be "dark", "transparent" or "normal"
 
-      sidebars = "dark", -- style for sidebars, see below
-      floats = "transparent", -- style for floating windows
-    },
-    sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-    day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-    hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-    dim_inactive = false, -- dims inactive windows
-    lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
-  })
-
-vim.cmd[[colorscheme tokyonight]]
 
   require'marks'.setup {
     mappings = {
